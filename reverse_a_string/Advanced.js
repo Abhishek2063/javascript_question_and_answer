d3fd9ca9645bbd9429b551b8bc2f6bad2d5dfbc4
abhishek <abhishekgarg2063@gmail.com>
2024-02-20 17:29:02 +0530

    algined

--- reverse_a_string/Advanced.js
+++ reverse_a_string/Advanced.js
@@ -1,7 +1,10 @@
 // Advanced level
 const reverseString = (str) => {
-    console.log([...str]); // Output: [ 'h', 'e', 'l', 'l', 'o' ]
-    console.log([...str].reverse()); // Output: [ 'o', 'l', 'l', 'e', 'h' ]
+    console.log([...str]); 
+    // Output: [ 'h', 'e', 'l', 'l', 'o' ]
+    console.log([...str].reverse()); 
+    // Output: [ 'o', 'l', 'l', 'e', 'h' ]
   return [...str].reverse().join("");
 };
-console.log(reverseString('hello')); // Output: 'olleh'
+console.log(reverseString('hello')); 
+// Output: 'olleh'
--- reverse_a_string/Intermediate.js
+++ reverse_a_string/Intermediate.js
@@ -1,8 +1,11 @@
 // Intermediate level
 const reverseString = (str) => {
-  console.log(str.split("")); // Output: [ 'h', 'e', 'l', 'l', 'o' ]
-  console.log(str.split("").reverse()); // Output: [ 'o', 'l', 'l', 'e', 'h' ]
+  console.log(str.split("")); 
+  // Output: [ 'h', 'e', 'l', 'l', 'o' ]
+  console.log(str.split("").reverse()); 
+  // Output: [ 'o', 'l', 'l', 'e', 'h' ]
 
   return str.split("").reverse().join("");
 };
-console.log(reverseString("hello")); // Output: 'olleh'
+console.log(reverseString("hello")); 
+// Output: 'olleh'
